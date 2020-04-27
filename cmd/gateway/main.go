a93460c62f9bcb333a9a5706429159cf93935c86
Tim Heckman <t@heckman.io>
2020-04-27 01:01:12 -0700

    Add a write to Redis on startup (to test that it works)

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/go-redis/redis"
 	_ "github.com/heroku/x/hmetrics/onload"
 
 	"github.com/theckman/gopher2/config"
@@ -26,6 +28,25 @@ func main() {
 		}
 		_, _ = io.WriteString(w, "Hello World!")
 	})
+
+	redisOpts := &redis.Options{
+		Network:      "tcp",
+		Addr:         cfg.Redis.Addr,
+		Password:     cfg.Redis.Password,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  11 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		PoolSize:     20,
+		MinIdleConns: 5,
+		PoolTimeout:  5 * time.Second,
+	}
+
+	c := redis.NewClient(redisOpts)
+	key := fmt.Sprintf("heartbeat:%s:%s", cfg.Heroku.AppName, cfg.Heroku.DynoID)
+	res := c.Set(key, time.Now().Unix(), 0)
+	if res.Err() != nil {
+		log.Printf("redis error: %v", res.Err())
+	}
 
 	serverAddr := fmt.Sprintf("0.0.0.0:%d", cfg.Port)
 
