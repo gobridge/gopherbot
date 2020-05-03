dfbea0c6de70db0e822ec504297e5c3861769766
Tim Heckman <t@heckman.io>
2020-05-02 21:57:26 -0700

    Restructure internal/gateway package to be more maintainable

--- internal/gateway/handlers.go
+++ internal/gateway/handlers.go
@@ -1,40 +1,18 @@
 package gateway
 
 import (
-	"bytes"
-	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"mime"
 	"net/http"
-	"time"
 
 	"github.com/rs/zerolog"
-	"github.com/theckman/gopher2/signing"
 	"github.com/theckman/gopher2/workqueue"
 	"github.com/valyala/fastjson"
 )
 
-type ctxKey uint8
-
-const (
-	ctxKeyReqID ctxKey = iota
-)
-
-func ctxRequestID(ctx context.Context) (string, bool) {
-	v := ctx.Value(ctxKeyReqID)
-	if v == nil {
-		return "", false
-	}
-
-	rid, ok := v.(string)
-	if !ok {
-		return "", false
-	}
-
-	return rid, true
-}
+const maxBodySize = 2 * 1024 * 1024 // 2 MB
 
 func (s *server) handleNotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
@@ -43,31 +21,6 @@ func (s *server) handleNotFound(w http.ResponseWriter, r *http.Request) {
 func (s *server) handleRUOK(w http.ResponseWriter, r *http.Request) {
 	_, _ = io.WriteString(w, "imok")
 }
-
-func chMiddlewareFactory(baseLogger zerolog.Logger, next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.Background()
-
-		if rid := r.Header.Get("X-Request-ID"); len(rid) > 0 {
-			ctx = context.WithValue(ctx, ctxKeyReqID, rid)
-			w.Header().Set("X-Request-ID", rid)
-		}
-
-		// Slack expects a response within 3 seconds, give ourselves 2.9 seconds
-		ctx, cancel := context.WithTimeout(ctx, 2900*time.Millisecond)
-
-		defer cancel()
-
-		next(w, r.WithContext(ctx))
-	}
-}
-
-const (
-	// TODO(theckman): make this a parameter?
-	slackTeamID = "T029RQSE6"
-
-	maxBodySize = 2 * 1024 * 1024 // 2 MB
-)
 
 func getJSONString(document *fastjson.Value, key string) (string, error) {
 	if !document.Exists(key) {
@@ -97,132 +50,6 @@ func getJSONInt64(document *fastjson.Value, key string) (int64, error) {
 	}
 
 	return v, nil
-}
-
-func slackSignatureMiddlewareFactory(hmacKey, token, appID string, baseLogger *zerolog.Logger, next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		lc := baseLogger.With()
-
-		rid, _ := ctxRequestID(r.Context())
-		lc = lc.Str("request_id", rid)
-
-		logger := lc.Str("context", "slack_middleware").Logger()
-
-		body, err := ioutil.ReadAll(io.LimitReader(r.Body, maxBodySize))
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to read request body")
-
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		r.Body = ioutil.NopCloser(bytes.NewReader(body))
-
-		// validate that the signature looks good
-		err = signing.Validate(hmacKey, signing.Request{
-			Body:      body,
-			Timestamp: r.Header.Get(signing.SlackTimestampHeader),
-			Signature: r.Header.Get(signing.SlackSignatureHeader),
-		})
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to validated Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		document, err := fastjson.ParseBytes(body)
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to unmarshal JSON document")
-
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			return
-		}
-
-		rToken, err := getJSONString(document, "token")
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if rToken != token {
-			logger.Error().
-				Str("error", "mismatched token").
-				Str("token", rToken).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		typeValue, err := getJSONString(document, "type")
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// the following items will NOT be present
-		// so let's skip them
-		if typeValue == "url_verification" {
-			next(w, r)
-			return
-		}
-
-		rAppID, err := getJSONString(document, "api_app_id")
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if rAppID != appID {
-			logger.Error().
-				Str("error", "mismatched api_app_id").
-				Str("api_app_id", rAppID).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		rTeamID, err := getJSONString(document, "team_id")
-		if err != nil {
-			logger.Error().
-				Err(err).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-		}
-
-		if rTeamID != slackTeamID {
-			logger.Error().
-				Str("error", "mismatched team_id").
-				Str("team_id", rTeamID).
-				Msg("failed to validate Slack request")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		next(w, r)
-	}
 }
 
 func requestValues(document *fastjson.Value) (eventType, eventID string, eventTimestamp int64, err error) {
