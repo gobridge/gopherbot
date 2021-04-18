c3ed82aeb6bcda6fafffb3f5e7b80eeaeb51fd5a
Christine Dodrill <me@christine.website>
2021-04-18 16:19:44 -0400

    Update responses.go

--- cmd/consumer/responses.go
+++ cmd/consumer/responses.go
@@ -228,6 +228,7 @@ func injectMessageResponses(ma *handler.MessageActions) {
 		`- <https://rakyll.org/style-packages/>`,
 		`- <https://medium.com/@benbjohnson/standard-package-layout-7cdbc8391fc1#.ds38va3pp>`,
 		`- <https://peter.bourgon.org/go-best-practices-2016/#repository-structure>`,
+		`- <https://christine.website/blog/within-go-repo-layout-2020-09-07>`,
 		``,
 		`This article will help you understand the design philosophy for packages: <https://www.goinggo.net/2017/02/design-philosophy-on-packaging.html>`,
 	)
